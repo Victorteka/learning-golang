0946a6c7020bc2ebd7a3862572d81fab1c8e84b9
Eugene Mutai <eugenemutai@gmail.com>
2015-06-21 13:49:35 +0300

    ordered them so as for easy for scavangers to understand my learning process

--- main.go
+++ main.go
@@ -1,13 +1,21 @@
+// This is know as a “package declaration”.
+// Every Go pro- gram must start with a package declaration.
+// Packages are Go's way of organizing and reusing code.
+// There are two types of Go programs: executables and libraries.
 package main
 
+// The import keyword is how we include code from other packages to use with our program.
+// The fmt package (shorthand for format) implements formatting for in- put and output.
+// The use of double quotes like this is known as a “string literal” which is a type of “expression”.
 import "fmt"
 
-func main() {
-    fmt.Print("Enter a number: ")
+// this is a comment
+// All functions start with the keyword func 
+// followed by the name of the function (main in this case)
+func main(int i = 3) {
+    var a string = "Hello world!!"
+    b := "Do not stay at home."
 
-    var input float64
-    fmt.Scanf("%f", &input)
-
-    output := input * 2
-    fmt.Println(output)
+    fmt.Println(a + b)
+    fmt.Println(true && false)
 }
