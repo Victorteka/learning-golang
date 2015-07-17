74e39d40799230f3d0ef8eaf16c690a5c4385e8c
Eugene Mutai <eugenemutai@gmail.com>
2015-07-18 00:07:21 +0300

    data types mods

--- data types/data_types.go
+++ data types/data_types.go
@@ -4,50 +4,54 @@ import "fmt"
 
 func main() {
 
-  const go_lang string = "GO Language"
+	const goLang string = "GO Language"
 
-  // ----------- NOTE: ---------------
-  // Do not declare values that you will not use
-  // The compiler fails
+	// ----------- NOTE: ---------------
+	// Do not declare values that you will not use
+	// The compiler fails
 
-  // my name
-  // var name string = "Eugene Mutai"
-  name := "Eugene Mutai"
+	// my name
+	// var name string = "Eugene Mutai"
+	name := "Eugene Mutai"
 
-  // age
-  var age int = 25
+	// age
+	var age int = 25
 
-  // float
-  var how_handsome float64 = 8.75
+	// float
+	var howHandsome float64 = 8.75
 
-  // arrays
-  fruits := [5]string{"apple", "oranges", "pinapple", "pears", "peaches"}
-  count := [3]int{1,2,3}
+	// arrays
+	fruits := [5]string{"apple", "oranges", "pinapple", "pears", "peaches"}
+	count := [3]int{1, 2, 3}
 
-  // object
-  aboutme := make(map[string]string)
-  aboutme["name"] = "Eugene Mutai"
-  // aboutme["age"] = "25"
-  // aboutme["location"] = "Woodley, Kilimani"
+	for index, fruit := range fruits {
+		fmt.Println(fruit)
+	}
 
-  // The other way of declaring an object
-  // always has a comma in the last declaration
-  // unlike JavaScript
-  more_about_me := map[string]string{
-       "name": "Eugene Mutai",
-      //  "age": "25",
-      //  "location": "Woodley, Kilimani",
-  }
+	// object
+	aboutme := make(map[string]string)
+	aboutme["name"] = "Eugene Mutai"
+	// aboutme["age"] = "25"
+	// aboutme["location"] = "Woodley, Kilimani"
 
-  // For multidimentional object checking
-  // Age will not be printed if it does not exist
-  if age, ok := aboutme["age"]; ok {
-      fmt.Println(age)
-  }
+	// The other way of declaring an object
+	// always has a comma in the last declaration
+	// unlike JavaScript
+	moreAboutMe := map[string]string{
+		"name": "Eugene Mutai",
+		//  "age": "25",
+		//  "location": "Woodley, Kilimani",
+	}
 
-  fmt.Println("Learning" + count)
-  fmt.Println(name, age, how_handsome, fruits)
-  fmt.Println(aboutme["name"], more_about_me["name"])
-  fmt.Println(count)
+	// For multidimentional object checking
+	// Age will not be printed if it does not exist
+	if age, ok := aboutme["age"]; ok {
+		fmt.Println(age)
+	}
+
+	fmt.Println("Learning" + count)
+	fmt.Println(name, age, howHandsome, fruits)
+	fmt.Println(aboutme["name"], moreAboutMe["name"])
+	fmt.Println(count)
 
 }
